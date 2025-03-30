bf533361b55727566bc30fc5c9cdb0e3cb12aa42
Aaron Chan <ChanAaron618@gmail.com>
2025-03-29 23:34:53 -0400

    Story Game lacking asthethics

--- app/_layout.js
+++ app/_layout.js
@@ -4,10 +4,12 @@ import CustomTabBar from '../components/CustomTabBar';
 export default function Layout() {
   return (
     <Tabs tabBar={(props) => <CustomTabBar {...props} />}>
-      <Tabs.Screen name="index" options = {{ title: "Games"}} />
+            <Tabs.Screen name="survivorTipsPage" options={{ title: "Tips" }} />
       <Tabs.Screen name="survivorTipsPage" options={{ title: "Tips" }} />
       <Tabs.Screen name="animalLogPage" options={{ title: "Animal Log" }} />
       <Tabs.Screen name="compassPage" options={{ title: "Compass" }} />
+      <Tabs.Screen name="index" options = {{ tabBarItemStyle: {display: 'none'}}} />
+
     </Tabs>
   );
 }

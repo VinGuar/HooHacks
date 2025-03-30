4c75839fec0892aa4cd39166ee152286ffc9141d
Aaron Chan <ChanAaron618@gmail.com>
2025-03-29 22:58:45 -0400

    Story Game in Progress

--- app/_layout.js
+++ app/_layout.js
@@ -4,8 +4,8 @@ import CustomTabBar from '../components/CustomTabBar';
 export default function Layout() {
   return (
     <Tabs tabBar={(props) => <CustomTabBar {...props} />}>
+      <Tabs.Screen name="index" options = {{ title: "Games"}} />
       <Tabs.Screen name="survivorTipsPage" options={{ title: "Tips" }} />
-      <Tabs.Screen name="index" options={{ title: "Quiz" }} />
       <Tabs.Screen name="animalLogPage" options={{ title: "Animal Log" }} />
       <Tabs.Screen name="compassPage" options={{ title: "Compass" }} />
     </Tabs>
